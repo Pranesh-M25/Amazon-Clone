eb80487a22eda68f6884f551d29e57a0ad7d6c3d
Pranesh-M25 <pranesh25m@gmail.com>
2025-03-25 18:41:14 +0530

    second commit

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -119,34 +119,36 @@ document.querySelectorAll('.js-update-link').forEach((link) => {
   link.addEventListener('click', () => {
     const productId = link.dataset.productId;
     const container = document.querySelector(`.js-cart-item-container-${productId}`);
-    if (container) {
+    
       container.classList.add('is-editing-quantity');
-    } else {
-      console.error(`Container not found for productId: ${productId}`);
-    }
+    
   });
 });
 
 document.querySelectorAll('.js-save-link').forEach((link) => {
   link.addEventListener('click', () => {
     const productId = link.dataset.productId;
-    const container = document.querySelector(`.js-cart-item-container-${productId}`);
-    if (container) {
-      container.classList.remove('is-editing-quantity');
-    } else {
-      console.error(`Container not found for productId: ${productId}`);
-    }
+   
+    
 
     const quantityInput = document.querySelector(`.js-quantity-input-${productId}`);
     const newQuantity = Number(quantityInput.value);
+
+    if(newQuantity < 0 || newQuantity >= 1000){
+      alert('Quantity must be at least 0 and less than 1000');
+      return;
+    }
     updateQuantity(productId, newQuantity);
 
+    const container = document.querySelector(
+      `.js-cart-item-container-${productId}`
+    );
+    container.classList.remove('is-editing-quantity');
+
     const quantityLabel = document.querySelector(`.js-quantity-label-${productId}`);
-    if (quantityLabel && newQuantity >=0) {
+   
       quantityLabel.innerHTML = newQuantity;
-    } else {
-      console.error(`Quantity label not found for productId: ${productId}`);
-    }
+    
 
     
     
