33b9bb2b41aa9d5c99682edad253e714ed6f8f7b
Pranesh-M25 <pranesh25m@gmail.com>
2025-03-25 23:18:48 +0530

    changed delivery date header

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -27,10 +27,27 @@ cart.forEach((cartItem) => {
     }
   });
 
+  const deliveryOptionId = cartItem.deliveryOptionId;
+
+  let deliveryOption;
+
+  deliveryOptions.forEach((option) => {
+    if (option.id === deliveryOptionId){
+      deliveryOption = option;
+    }
+  });
+  const today = dayjs();
+  const deliveryDate = today.add(
+    deliveryOption.deliveryDays,
+    'days'
+  );
+
+  const dateString=deliveryDate.format('dddd, MMMM D');
+
   cartSummaryHTML += `
     <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
       <div class="delivery-date">
-        Delivery date: Tuesday, June 21
+        Delivery date: ${dateString}
       </div>
       <div class="cart-item-details-grid">
         <img src="${matchingProduct.image}" class="product-image">
@@ -61,7 +78,7 @@ cart.forEach((cartItem) => {
           <div class="delivery-options-title">
             Choose a delivery option:
           </div>
-         ${deliveryOptionsHTML(matchingProduct)}
+         ${deliveryOptionsHTML(matchingProduct,cartItem)}
         </div>
       </div>
     </div>
@@ -70,7 +87,7 @@ cart.forEach((cartItem) => {
 
 
 
-function deliveryOptionsHTML(matchingProduct) {
+function deliveryOptionsHTML(matchingProduct,cartItem) {
   let html ='';
   deliveryOptions.forEach((deliveryOption) => {
     const today = dayjs();
@@ -84,10 +101,13 @@ function deliveryOptionsHTML(matchingProduct) {
     const priceString = deliveryOption.priceCents === 0
     ?  'Free'
     :  `${currencyFormat(deliveryOption.priceCents)} `;
+
+    const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
+
  html +=
     ` 
           <div class="delivery-option">
-            <input type="radio" class="delivery-option-input" name="delivery-option-${matchingProduct.id}">
+            <input type="radio" ${isChecked ?'checked'  : ''} class="delivery-option-input" name="delivery-option-${matchingProduct.id}">
             <div>
               <div class="delivery-option-date">
                ${dateString}
