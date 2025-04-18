3bb014de0d3fa676e9d02d9f3e5a6a3874f0c667
Pranesh-M25 <pranesh25m@gmail.com>
2025-04-18 21:41:30 +0530

    got introduced to backend

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,6 +2,7 @@ import { renderOrderSummary } from "./Checkout/ordersummary.js";
 import { renderPaymentSummary } from "./Checkout/paymentSummary.js";
 import { renderCheckoutHeader } from "./Checkout/checkoutHeader.js";
 //import './cart-class.js'
+import './backend-practice.js';
 
 renderCheckoutHeader();
 renderOrderSummary();
