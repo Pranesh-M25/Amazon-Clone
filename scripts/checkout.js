e53da318cea97b510ccd692551061448343fde5d
Pranesh-M25 <pranesh25m@gmail.com>
2025-04-22 19:13:30 +0530

    introduced promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,11 +2,34 @@ import { renderOrderSummary } from "./Checkout/ordersummary.js";
 import { renderPaymentSummary } from "./Checkout/paymentSummary.js";
 import { renderCheckoutHeader } from "./Checkout/checkoutHeader.js";
 import { loadProducts } from "./products.js";
+import { loadCart } from "./cart.js";
 //import './cart-class.js'
 //import './backend-practice.js';
 
-loadProducts(() => {
-  renderCheckoutHeader();
+new Promise((resolve) => {
+  
+  loadProducts(() => {
+    
+    resolve();
+  });
+}).then(() => {
+  return new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  });
+ 
+}).then(() =>  {
   renderOrderSummary();
+  renderCheckoutHeader();
   renderPaymentSummary();
-});
+}); //a promise contains a inner function and the function runs immediately when the promise is created, and promise is a class. THe resolve parameter is also a function that let us to control when to go to the next step. .then is the next step that needs to be excecuted when we call resolve(). 
+
+/* loadProducts(() => {
+  loadCart(() => {
+    renderOrderSummary();
+    renderCheckoutHeader();
+    renderPaymentSummary();
+  });
+  }); */
+ 
