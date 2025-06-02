8ac69e4508849e73c4f5e5796aabec690b05d1fe
Pranesh-M25 <pranesh25m@gmail.com>
2025-06-02 12:26:13 +0530

    made order button interactive and add backend functions to order

--- scripts/Checkout/paymentSummary.js
+++ scripts/Checkout/paymentSummary.js
@@ -2,6 +2,7 @@ import { cart } from "../cart.js";
 import { getProduct } from "../products.js";
 import { getDeliveryOption } from "../deliveryOptions.js";
 import { formatCurrency } from "../Utils/money.js";
+import { addOrder } from "../orders.js";
 
 export function renderPaymentSummary() {
   let productPriceCents = 0;
@@ -55,7 +56,7 @@ export function renderPaymentSummary() {
       <div class="payment-summary-money">$${formatCurrency(totalCents)}</div>
     </div>
 
-    <button class="place-order-button button-primary">
+    <button class="place-order-button button-primary js-place-order">
       Place your order
     </button>
   `;
@@ -63,4 +64,20 @@ export function renderPaymentSummary() {
 
   document.querySelector('.js-payment-summary')
     .innerHTML = paymentSummaryHTML;
+
+  document.querySelector('.js-place-order')
+    .addEventListener('click', async () => {
+     const response = await fetch('https://supersimplebackend.dev/orders', {
+        method : 'POST',
+        headers : {
+          'Content-Type' : 'application/json'
+        },
+        body: JSON.stringify({
+          cart: cart
+        })
+      });
+
+      const order = await response.json();
+      addOrder(order);
+    } );
 }
